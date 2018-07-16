04b7390d44e67e601bd0066aeb2f092fe0ab5830
ChenArno <517625126@qq.com>
2018-07-16 16:23:18 +0800

    firts commit

---
+++ SerialPortLibrary/src/main/java/com/kongqw/serialportlibrary/ByteUtil.java
@@ -0,0 +1,24 @@
+package com.kongqw.serialportlibrary;
+
+public class ByteUtil {
+
+    /**
+     * 字节数组转换成对应的16进制表示的字符串
+     *
+     * @param src
+     * @return
+     */
+    public static String bytes2HexStr(byte[] src) {
+        StringBuilder builder = new StringBuilder();
+        if (src == null || src.length <= 0) {
+            return "";
+        }
+        char[] buffer = new char[2];
+        for (int i = 0; i < src.length; i++) {
+            buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
+            buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
+            builder.append(buffer);
+        }
+        return builder.toString().toUpperCase();
+    }
+}
---
+++ app/src/main/java/port/vanz/com/port/MainActivity.java
@@ -0,0 +1,49 @@
+package port.vanz.com.port;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.kongqw.serialportlibrary.ByteUtil;
+import com.kongqw.serialportlibrary.Device;
+import com.kongqw.serialportlibrary.SerialPortFinder;
+import com.kongqw.serialportlibrary.SerialPortManager;
+import com.kongqw.serialportlibrary.listener.OnSerialPortDataListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "====>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SerialPortManager serialPortManager = new SerialPortManager();
+        serialPortManager.setOnSerialPortDataListener(new OnSerialPortDataListener() {
+            @Override
+            public void onDataReceived(byte[] bytes) {
+                Log.i(TAG, "onDataReceived: "+ ByteUtil.bytes2HexStr(bytes));
+            }
+
+            @Override
+            public void onDataSent(byte[] bytes) {
+
+            }
+        });
+
+        SerialPortFinder serialPortFinder = new SerialPortFinder();
+        ArrayList<Device> devices = serialPortFinder.getDevices();
+        for(Device device : devices){
+            if(device.getName().equals("ttyS3")){
+               Boolean b = serialPortManager.openSerialPort(device.getFile(),9600);
+               Log.i(TAG, "onCreate: "+b);
+            }
+        }
+
+    }
+
+
+}
